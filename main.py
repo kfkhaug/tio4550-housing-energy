854506436764438647684fd4baedc60ba5580cd5
Kristian Fredrik Kjeldsen Haug <haug.fredrik@gmail.com>
2025-09-11 19:04:48 +0200

    Removed main.py, added some dev packages, adjusted the dev docs.

--- main.py
+++
@@ -1,9 +0,0 @@
-# TODO Telete this file. Remember to change instruction in docs/dev_setup.md afterwards.
-
-
-def main():
-    print("Hello from tio4550-housing-energy!")
-
-
-if __name__ == "__main__":
-    main()
